d621d20faf7d05888f7e7e7af5404b3a3b87c426
evianojay <wfshackles@gmail.com>
2025-07-26 20:48:12 +0200

    Fix import paths for recipeStore

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,21 +1,20 @@
 // src/recipeStore.js
 import { create } from 'zustand';
-import { nanoid } from 'nanoid';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (recipe) =>
     set((state) => ({
-      recipes: [...state.recipes, { ...recipe, id: nanoid() }],
-    })),
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      recipes: [...state.recipes, recipe],
     })),
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 }));
