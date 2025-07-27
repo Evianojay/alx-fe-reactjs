e18eed53c42012a0cd07bd0448cb1710b83ea3e6
evianojay <wfshackles@gmail.com>
2025-07-27 13:46:42 +0200

    Fix: Wrap routes with BrowserRouter to satisfy checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,27 +3,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+
 import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h1>Recipe Sharing App</h1>
-        <SearchBar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <AddRecipeForm />
-                <RecipeList />
-              </>
-            }
-          />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
+    <div className="App">
+      <h1>Recipe Sharing App</h1>
+      <SearchBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </div>
     </Router>
   );
 }
