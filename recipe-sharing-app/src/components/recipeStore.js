5ea4419976bf18aec027fc340b10c4d7bec1159d
evianojay <wfshackles@gmail.com>
2025-07-27 13:20:30 +0200

    Add updateRecipe method to Zustand store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,33 +1,36 @@
-// src/recipeStore.js
+// src/components/recipeStore.js
 import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
   favorites: [],
   
-  // Add this function!
   setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 
-  addRecipe: (recipe) => set((state) => ({
-    recipes: [...state.recipes, recipe]
-  })),
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
 
-  editRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map((r) =>
-      r.id === updatedRecipe.id ? updatedRecipe : r
-    )
-  })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
 
-  deleteRecipe: (id) => set((state) => ({
-    recipes: state.recipes.filter((r) => r.id !== id)
-  })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === updatedRecipe.id ? updatedRecipe : r
+      ),
+    })),
 
-  toggleFavorite: (id) => set((state) => {
-    const isFavorite = state.favorites.includes(id);
-    return {
-      favorites: isFavorite
-        ? state.favorites.filter((fid) => fid !== id)
-        : [...state.favorites, id],
-    };
-  }),
+  toggleFavorite: (id) =>
+    set((state) => {
+      const isFavorite = state.favorites.includes(id);
+      return {
+        favorites: isFavorite
+          ? state.favorites.filter((favId) => favId !== id)
+          : [...state.favorites, id],
+      };
+    }),
 }));
