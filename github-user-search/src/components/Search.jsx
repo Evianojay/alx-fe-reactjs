b55b6eaba8fc0971af518ec8025823c5ae83ff4d
evianojay <wfshackles@gmail.com>
2025-08-03 07:57:44 +0200

    Fix error message for checker compliance

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -38,7 +38,7 @@ export default function Search() {
       </form>
 
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user</p>}
+      {error && <p>Looks like we cant find the user</p>}
       {user && (
         <div>
           <img src={user.avatar_url} alt={user.login} width={100} />
