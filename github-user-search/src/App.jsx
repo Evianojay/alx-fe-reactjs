7224ded6b3cf8e78c8aced0c401496297033bc66
evianojay <wfshackles@gmail.com>
2025-08-03 07:10:29 +0200

    Task 1: Basic GitHub user search with API integration

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-
-
+import Search from "./components/search";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>GitHub User Search</h1>
-      <p>Project setup successful 🎉</p>
+      <Search />
     </div>
   );
 }
+
 export default App;
+
