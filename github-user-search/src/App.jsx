c00440c0c01756b7cc4f9622bf762d6645a3d1c7
evianojay <wfshackles@gmail.com>
2025-08-03 11:32:23 +0200

    Task 0: Initial project setup for GitHub User Search Application

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Search from './components/search';
 
 function App() {
   return (
-    <div className="App">
-      <h1>GitHub User Search</h1>
-      <Search />
+    <div className="min-h-screen bg-gray-100 text-center p-8">
+      <h1 className="text-2xl font-bold mb-4">GitHub User Search</h1>
+      <p>Start typing to search GitHub users...</p>
     </div>
   );
 }
 
 export default App;
-
--- github-user-search/src/services/githubService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "https://api.github.com";
-
-export async function fetchUserData(username) {
-  const response = await axios.get(`${BASE_URL}/users/${username}`);
-  return response.data;
-}
